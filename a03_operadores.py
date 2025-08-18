330845df301e35be388ed7f353271696c75d1f5a
Abel Soto <abel.soto2009@gmail.com>
2025-08-17 21:01:02 -0400

    Operadores

---
+++ a03_operadores.py
@@ -0,0 +1,28 @@
+#Analizaremos los operadores que soporta python.
+"""
+Operadores
+Suma, resta, multiplicacion, division.
+
+"""
+#suma
+print("La suma de 5 mas 4 :", 5 + 4)
+
+numero1 = 10
+numero2 = 5
+print("La suma de 5 mas 4 version 2 :", numero1 + numero2)
+resultado = numero1 + numero2
+print("La suma de 5 mas 4 version 3:", resultado)
+
+print("resultado: ", 10 + 5)
+print("resultado: ", 10 - 5)
+print("resultado: ", 10 * 5)
+print("resultado: ", 10 % 5)
+print("resultado: ", 10 // 5)
+print("resultado **: ", 10 ** 3)
+
+print("Hola"+"Python")
+print("Hola"+"Python"+"Como Estas?")
+print("Esto es: " + str(5))
+
+
+
